6b9b3ebd3013f38e6c87c06698668f1bfa7e0b7b
treppenwitz03 <owenlegrande@gmail.com>
2024-12-03 17:13:31 +0800

    Hello

--- src/main.py
+++ src/main.py
@@ -3,69 +3,67 @@ import os
 
 import views
 
-class Main(ft.Page):
-    def __init__(self, page: ft.Page):
-        self = page
-        self.bgcolor = "#fffada"
-        self.window.title_bar_hidden = False if "ANDROID_BOOTLOGO" in os.environ else True
-        self.fonts = {
-            "Bahnschrift" : "fonts/Bahnschrift.ttf"
-        }
+def main(page: ft.Page):
+    page.bgcolor = "#fffada"
+    page.window.title_bar_hidden = False if "ANDROID_BOOTLOGO" in os.environ else True
+    page.fonts = {
+        "Bahnschrift" : "fonts/Bahnschrift.ttf"
+    }
 
-        self.theme = ft.Theme(
-            font_family= "Bahnschrift"
-        )
+    page.theme = ft.Theme(
+        font_family= "Bahnschrift"
+    )
 
-        opening_view = views.OpeningView()
+    opening_view = views.OpeningView()
 
-        page_switcher = ft.AnimatedSwitcher(
-            opening_view,
-            duration=250,
-            reverse_duration=250
-        )
+    page_switcher = ft.AnimatedSwitcher(
+        opening_view,
+        duration=250,
+        reverse_duration=250
+    )
 
-        def minimize():
-            self.window.minimized = True
-            self.update()
-        
-        def maximize():
-            self.window.maximized = not self.window.maximized
-            self.update()
+    def minimize():
+        page.window.minimized = True
+        page.update()
+    
+    def maximize():
+        page.window.maximized = not page.window.maximized
+        page.update()
 
-        if not "ANDROID_BOOTLOGO" in os.environ:
-            self.add(ft.WindowDragArea(
-                ft.Row(
-                    alignment=ft.MainAxisAlignment.END,
-                    controls=[
-                        ft.IconButton(
-                            ft.CupertinoIcons.MINUS,
-                            icon_color=ft.Colors.BLACK,
-                            icon_size=24,
-                            width = 32,
-                            height = 32,
-                            on_click=lambda _: minimize()
-                        ),
-                        ft.IconButton(
-                            ft.CupertinoIcons.SQUARE,
-                            icon_color=ft.Colors.BLACK,
-                            icon_size=24,
-                            width = 32,
-                            height = 32,
-                            on_click=lambda _: maximize()
-                        ),
-                        ft.IconButton(
-                            ft.CupertinoIcons.XMARK,
-                            icon_color=ft.Colors.BLACK,
-                            icon_size=24,
-                            width = 32,
-                            height = 32,
-                            on_click=lambda _: self.window.close()
-                        )
-                    ]
-                )
-            ))
+    if not "ANDROID_BOOTLOGO" in os.environ:
+        page.add(ft.WindowDragArea(
+            ft.Row(
+                alignment=ft.MainAxisAlignment.END,
+                controls=[
+                    ft.IconButton(
+                        ft.CupertinoIcons.MINUS,
+                        icon_color=ft.Colors.BLACK,
+                        icon_size=24,
+                        width = 32,
+                        height = 32,
+                        on_click=lambda _: minimize()
+                    ),
+                    ft.IconButton(
+                        ft.CupertinoIcons.SQUARE,
+                        icon_color=ft.Colors.BLACK,
+                        icon_size=24,
+                        width = 32,
+                        height = 32,
+                        on_click=lambda _: maximize()
+                    ),
+                    ft.IconButton(
+                        ft.CupertinoIcons.XMARK,
+                        icon_color=ft.Colors.BLACK,
+                        icon_size=24,
+                        width = 32,
+                        height = 32,
+                        on_click=lambda _: page.window.close()
+                    )
+                ]
+            )
+        ))
 
-        self.add(page_switcher)
+    page.add(page_switcher)
 
 
-ft.app(Main, assets_dir="assets", view=ft.AppView.WEB_BROWSER)
+ft.app(main, assets_dir="assets")
--- src/views/opening_view.py
+++ src/views/opening_view.py
@@ -4,22 +4,21 @@ class OpeningView(ft.Container):
     def __init__(self):
         super().__init__()
 
-        icon = ft.Image(
+        image = ft.Image(
             src="lottie/neobrutalist.jpg",
-            fit=ft.ImageFit.CONTAIN,
             width=300,
             height=300
         )
 
-        text = ft.Text("Calculate your Grades and Know your Rank.", weight=ft.FontWeight.BOLD, size=32)
+        label = ft.Text("Calculate your Grades and Know your Rank.", weight=ft.FontWeight.BOLD, size=32)
 
         self.login_button = NeuButton("Login", "#ecc7d3")
         self.signup_button = NeuButton("Signup", "#48aaad")
 
         self.content = ft.Column(
             controls=[
-                icon,
-                text,
+                image,
+                label,
                 self.login_button,
                 self.signup_button
             ],
@@ -30,13 +29,13 @@ class OpeningView(ft.Container):
 class NeuButton(ft.GestureDetector):
     def __init__(self, label: str, col: str):
         super().__init__()
-        container = ft.Container()
-        container.content = ft.Text(label, weight=ft.FontWeight.BOLD, color="black", text_align=ft.TextAlign.CENTER)
-        container.bgcolor = col
-        container.border_radius = 12
-        container.border = ft.border.all(2, ft.Colors.BLACK)
-        container.padding = ft.padding.all(8)
-        container.animate_offset = ft.animation.Animation(250)
+        cont = ft.Container()
+        cont.content = ft.Text(label, weight=ft.FontWeight.BOLD, color="black", text_align=ft.TextAlign.CENTER)
+        cont.bgcolor = col
+        cont.border_radius = 12
+        cont.border = ft.border.all(2, ft.Colors.BLACK)
+        cont.padding = ft.padding.all(8)
+        cont.animate_offset = ft.animation.Animation(250)
 
         self.shadow = ft.BoxShadow(
             blur_radius=12,
@@ -45,13 +44,13 @@ class NeuButton(ft.GestureDetector):
             blur_style=ft.ShadowBlurStyle.SOLID,
         )
 
-        container.shadow = self.shadow
+        cont.shadow = self.shadow
 
-        container.on_hover = self.__hover__
+        cont.on_hover = self.__hover__
         self.on_tap_down = self.__pressed__
         self.on_tap_up = self.__released__
 
-        self.content = container
+        self.content = cont
     
     def __hover__(self, event: ft.HoverEvent):
         self.content.opacity = 0.8 if event.data == "true" else 1.0
